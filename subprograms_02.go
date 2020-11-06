94a8156b209f3f542c66f7d5c8f453c923c1b6cc
tjshee39 <gksh054@naver.com>
2020-11-06 15:15:35 +0900

    version2

---
+++ subprograms_02.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func sum(numbers ...int) int{
+	r := 0
+	for _ , number := range numbers {
+		r = r + number
+	}
+	return r
+}
+
+func main(){
+	//var inhaArray [7] string //array
+//	var inhaSlice [] string //slice
+//	inhaSlice = make([]string, 7)
+	inhaSlice := []string{"i","n","h","a" }
+	oneTOfive := []int{1, 2, 3, 4, 5}
+
+//	inhaSlice[0] = "i"
+//	inhaSlice[1] = "n"
+//	inhaSlice[2] = "h"
+//	inhaSlice[3] = "a"
+
+	for i := 0 ; i < len(inhaSlice); i++ {
+		fmt.Println(inhaSlice[i])
+	}
+
+
+	fmt.Println(sum(1,5,3))
+	fmt.Println(sum(3,1))
+	fmt.Println(sum(oneTOfive...))
+
+//	fmt.Println(1)
+}
